cd0f11c65437b7feb30cdcffd97871c4ac270552
Kristopher Cowley <kristopherleecowley@gmail.com>
2015-09-26 19:41:49 -0600

    Built basic page. Buttons not working yet.

--- app.js
+++ app.js
@@ -4,5 +4,45 @@ app.controller('MainController', MainController);
 
 function MainController() {
     var vm = this; //instead of using this when refering to the controller, let's use vm. It will make things easier.
+    
+    vm.joe = new Guy("Joe", 100);
+    vm.bob = new Guy("Bob", 150);
+    vm.bank = 200;
 
+    function Guy(name, startingCash) {
+        this.name = name;
+        this.cash = startingCash;
+        this.giveCash = function (amount) {
+            if (amount >= this.cash && amount > 0) {
+                this.cash -= amount;
+                return amount;
+            } else {
+                alert("I don't have enough cash to give you!" + this.name + " says.");
+                return 0;
+            }
+
+        }
+        this.receiveCash = function (amount) {
+            if (amount > 0) {
+                this.cash += amount;
+                return amount;
+
+            } else {
+                alert("Not so fast there, that numbers negative!" + this.name + " says.");
+                return 0;
+
+
+            }
+        };
+    };
+    vm.giveMoneyToJoe = function () {
+        if (vm.bank >= 10) {
+            vm.bank -= vm.joe.receiveCash(10);
+        } else {
+            alert("The bank is out of money!!!!!")
+        }
+    }
+    vm.receiveMoneyFromBob = function () {
+        vm.bank += vm.bob.giveCash(5);
+    }
 }
