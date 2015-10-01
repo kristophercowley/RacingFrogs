7a383b3b51e098716e086b6e320020e5877e231c
Kristopher Cowley <kristopherleecowley@gmail.com>
2015-10-01 17:46:03 -0600

    Updated styling, automatic frog racing on click

--- app.js
+++ app.js
@@ -2,47 +2,74 @@ var app = angular.module('racingFrogs', []);
 app.controller('MainController', MainController);
 //No need to change anything above this line.
 
-function MainController() {
+function MainController($timeout) {
     var vm = this; //instead of using this when refering to the controller, let's use vm. It will make things easier.
     
-    vm.frogs = [new Frog('Kermit', 50),new Frog('Freddy', 10),new Frog('Fraggle',0),new Frog('Dread',14)];
-    
+    vm.frogs = [new Frog('Kermit', 50), new Frog('Michigan J.', 10), new Frog('Mr. Toad', 0), new Frog('Hypnotoad', 14)];
+
     function Frog(name, posX) {
         this.name = name;
-        this.posX = 0;   
+        this.posX = 0;
     }
-    
-    vm.addFrog = function(name){
+
+    vm.addFrog = function (name) {
         //console.log("working")
-       vm.frogs.push(new Frog(name,0));
+        vm.frogs.push(new Frog(name, 0));
     }
-    
-    //setTimeout(vm.frogs.forEach,3000);
-    
-    vm.race = function(){
-       // console.log("Working")
-       //while(vm.isRacing)
-       vm.frogs.forEach(function(frog){
-           if(frog.posX < 100){
-           frog.posX += Math.floor(Math.random() * 10);
-           };
-           //vm.setTimout(vm.frogs.forEach,3000);
-           if(frog.posX >= 100){
-               alert(frog.name + "," + "Wins!");  
-               //vm.isRacing = false;
-           }
-       })
-      
+
+    vm.start = function () {
+        vm.reset();
+        vm.racing = true;
+        vm.race();
     }
-    
-    
-    vm.reset = function(){
-       // console.log("Working")
+
+    function _checkWin(){
         vm.frogs.forEach(function(frog){
-            frog.posX = 0;
+         if (frog.posX >= 91) {
+                    alert(frog.name + "," + "Wins!");
+                    vm.racing = false;
+                }   
         })
     }
     
+    vm.race = function () {
+        _checkWin();
+        if (vm.racing) {
+            vm.frogs.forEach(function (frog) {
+                if (frog.posX < 100) {
+                    frog.posX += Math.floor(Math.random() * 10);
+                };
+
+                // if (frog.posX >= 98) {
+                //     alert(frog.name + "," + "Wins!");
+                //     vm.racing = false;
+                // }
+            })
+        $timeout(vm.race, 300);            
+        }
+         
+        // vm.racing = true;
+        // while(vm.racing)
+        // vm.frogs.forEach(function(frog){
+        //    if(frog.posX < 100){
+        //    frog.posX += Math.floor(Math.random() * 10);
+        //    };
+          
+        //    if(frog.posX >= 98){
+        //        alert(frog.name + "," + "Wins!");  
+        //        vm.racing = false;
+        //    }
+        // })
+    }
+
+
+    vm.reset = function () {
+        // console.log("Working")
+        vm.frogs.forEach(function (frog) {
+            frog.posX = 0;
+        })
+    }
+
     vm.joe = new Guy("Joe", 100);
     vm.bob = new Guy("Bob", 150);
     vm.bank = 200;
