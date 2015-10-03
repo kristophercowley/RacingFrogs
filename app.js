eb0a8ca6bdc811991cee594e0be37f3eee91ba8b
Kristopher Cowley <kristopherleecowley@gmail.com>
2015-10-02 22:09:28 -0600

    comments

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module('racingFrogs', []);
 app.controller('MainController', MainController);
-//No need to change anything above this line.
+//No need to change anything above this line.//
 
 function MainController($timeout) {
     var vm = this; //instead of using this when refering to the controller, let's use vm. It will make things easier.
